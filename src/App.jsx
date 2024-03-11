c20e407c024975fe502b2d4043f8d70df14ae443
forxtoisate <hwoolford@gmail.com>
2024-03-11 15:55:54 -0500

    added styling to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import './App.css'
+
 import { Outlet } from "react-router-dom";
 
 import NavBar from './components/NavBar'
 
+
+
 function App() {
   
   return (
-    <>
+    <div className='app-container'>
       <NavBar />
+      <div className='main-content'>
       <Outlet />
-    </>
+      </div>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './index.css'
 
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Home from './pages/Home.jsx'
+import Projects from './pages/Projects.jsx'
+import Resume from './pages/Resume.jsx'
+
+const router = createBrowserRouter([
+{
+  path: '/',
+  element: <App />,
+  children: [
+    {
+      index: true,
+      element: <Home />
+    },
+    {
+      path: 'about',
+      element: <About />
+    },
+    {
+      path: 'projects',
+      element: <Projects />
+    },
+    {
+      path: 'contact',
+      element: <Contact />
+    },
+    {
+      path: 'resume',
+      element: <Resume />
+    },
+  ]
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
