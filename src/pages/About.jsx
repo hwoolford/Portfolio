e7ccf96339c94a1abab15150d9e9cd610afb2251
forxtoisate <hwoolford@gmail.com>
2024-03-11 16:16:04 -0500

    Updated h1 style and about.jsx title

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
-        <div className="about">
-            <h1>About</h1>
+        <div className="about-container">
+            <h1 className="text-center heading">About Me</h1>
+            <div className="main d-flex flex-row justify-content-between align-items-center"></div>
         </div>
     )
 }
