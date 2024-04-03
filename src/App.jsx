54699706cdc218708efa60256aff1a286ed4d1fe
forxtoisate <hwoolford@gmail.com>
2024-04-03 16:12:34 -0500

    Changing up the NavBar with materialUI. Testing it out

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 
 import { Outlet } from "react-router-dom";
 
-import NavBar from './components/NavBar.jsx'
+// import NavBar from './components/NavBar.jsx'
+// import NavBar2 from './components/NavBar2.jsx'
+import NavBar3 from './components/NavBar3.jsx'
 import Footer from './components/Footer.jsx';
 
 
@@ -11,7 +13,9 @@ function App() {
   
   return (
     <div className='app-container'>
-      <NavBar />
+      <NavBar3 />
+      {/* <NavBar /> */}
+      {/* <NavBar2 /> */}
       <div className='main-content'>
       <Outlet />
       </div>
---
+++ src/components/NavBar3.jsx
@@ -0,0 +1,131 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = [
+  { title: "About Me", path: "/about" },
+  { title: "Projects", path: "/projects" },
+  { title: "Contact", path: "/contact" },
+  { title: "Resume", path: "/resume" },
+];
+
+export default function NavBar3() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "#1f1f1f",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h1"
+            noWrap
+            component={Link}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Francois One",
+              fontWeight: 400,
+              fontSize: "3.5rem",
+              color: "#219ebc",
+              textDecoration: "none",
+            }}
+          >
+            HEATHER WOOLFORD
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.title} onClick={handleCloseNavMenu}>
+                  <Link to={page.path} style={{ textDecoration: "none" }}>
+                    <Typography textAlign="center">{page.title}</Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h2"
+            noWrap
+            component={Link}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "Francois One",
+              fontWeight: 400,
+              fontSize: "3.5rem",
+              color: "#219ebc",
+              textDecoration: "none",
+            }}
+          >
+            HEATHER WOOLFORD
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.title}
+                component={Link}
+                to={page.path}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page.title}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
