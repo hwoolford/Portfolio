cd83486517f7b87265b4100cf7431382a7ba3234
forxtoisate <hwoolford@gmail.com>
2024-04-05 15:33:16 -0500

    Updated contact page

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -5,15 +5,20 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function ContactInfo() {
+    const email = 'hwoolford@gmail.com';
+    const github = 'https://github.com/hwoolford'
+    const linkdin= 'https://www.linkedin.com/in/hwoolford/'
   return (
     <div className="contact-info-holder">
-    <h2 className="contact-me">Contact Me</h2>
+    <h2 className="contact-me">Connect with Me</h2>
       <Typography variant="h1" fontSize="3.5rem" fontFamily="Quicksand">
         Heather Woolford
       </Typography>
       <Button
       className="contact-details"
         startIcon={<EmailIcon style={{ fontSize: "2.5rem" }} />}
+        component="a" 
+      href={`mailto:${email}`} 
       >
         hwoolford@gmail.com
       </Button>
@@ -21,14 +26,16 @@ export default function ContactInfo() {
       <Button
       className="contact-details"
         startIcon={<GitHubIcon style={{ fontSize: "2.5rem" }} />}
-      >
-        github.com/hwoolford
+        component="a"
+        href={github} target="_blank" rel="noreferrer" >
+       github.com/hwoolford
       </Button>
       <br />
       <Button
       className="contact-details"
         startIcon={<LinkedInIcon style={{ fontSize: "2.5rem" }} />}
-      >
+        component="a"
+        href={linkdin} target="_blank" rel="noreferrer">
         linkedin.com/in/hwoolford
       </Button>
     </div>
