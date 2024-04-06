f87e247a7af2f80a730ac054c7b1cd7d1a3146c8
forxtoisate <hwoolford@gmail.com>
2024-04-06 14:42:23 -0500

    changed back to html button to see if submit works

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,14 +4,11 @@ import "../ContactForm/ContactForm.css";
 import { useState } from "react";
 import {
   Box,
-  Button,
+  // Button,
   Grid,
 } from "@mui/material/";
-import SendIcon from "@mui/icons-material/Send";
+// import SendIcon from "@mui/icons-material/Send";
 import { validateEmail } from "../../utils/validateEmail";
-
-
-
 
 export default function ContactForm() {
   const [name, setName] = useState("");
@@ -30,12 +27,6 @@ export default function ContactForm() {
     }
   };
 
-  // const encode = (data) => {
-  //   return Object.keys(data)
-  //       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-  //       .join("&");
-  // }
-
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
@@ -47,113 +38,108 @@ export default function ContactForm() {
       return;
     }
 
-    console.log(errors)
+    console.log(errors);
 
     setName("");
     setEmail("");
     setMessage("");
     setErrors({});
-
-    // fetch("/", {
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //   body: encode({ "form-name": "contact", ...this.state })
-    // })
-    //   .then(() => alert("Success!"))
-    //   .catch(error => alert(error));
-
-      
   };
-
 
   return (
     <>
       <h2 className="send-message">Send Me a Message</h2>
       <div className="form-container">
-          <Box
+        <Box
           container="true"
-            noValidate
-            autoComplete="off"
-            sx={{ flexGrow: 1 }}
-          > 
-            <form netlify-honeypot="bot-field" className="form" id="contact-form" name="contact" method="post" data-netlify="true" onSubmit={handleFormSubmit}>
-              <input type="hidden" name="form-name" value="contact" />
-              <Grid container spacing={3} justifyContent="center">
-                <Grid
-                  item
-                  xs={12}
-                  md={6}
-                  container
-                  justifyContent="center"
-                  alignItems="center"
-                 > 
-                  <input
-                    type="text"
-                    required
-                    className="inputBox"
-                    name="name"
-                    id="name"
-                    aria-label="Full Name"
-                    placeholder="Full Name"
-                    value={name}
-                    onChange={handleInputChange}
-                  ></input>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  md={6}
-                  container
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  <input
-                    type="email"
-                    required
-                    className="inputBox"
-                    name="email"
-                    id="email"
-                    aria-label="Email Address"
-                    placeholder="Email Address"
-                    value={email}
-                    onChange={handleInputChange}
-                  ></input>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  md={12}
-                  container
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  <textarea
-                    type="text"
-                    required
-                    className="inputBox"
-                    name="message"
-                    id="message"
-                    aria-label="Your Message"
-                    placeholder="Your Message"
-                    value={message}
-                    onChange={handleInputChange}
-                  ></textarea>
-                </Grid>
-                <div className="button-container">
-                    <Grid item xs={12} md={12} justifyContent="center" container>
-                    <Button
-                      variant="contained"
-                      type="submit"
-                      endIcon={<SendIcon style={{ fontSize: "2.75rem" }} />}
-                      >
-                        Send
-                      </Button>
-                    </Grid>
-                </div>
+          noValidate
+          autoComplete="off"
+          sx={{ flexGrow: 1 }}
+        >
+          <form
+            className="form"
+            name="contact"
+            method="post"
+            data-netlify="true"
+            onSubmit={handleFormSubmit}
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <Grid container spacing={3} justifyContent="center">
+              <Grid
+                item
+                xs={12}
+                md={6}
+                container
+                justifyContent="center"
+                alignItems="center"
+              >
+                <input
+                  type="text"
+                  required
+                  className="inputBox"
+                  name="name"
+                  id="name"
+                  aria-label="Full Name"
+                  placeholder="Full Name"
+                  value={name}
+                  onChange={handleInputChange}
+                ></input>
               </Grid>
-            </form>
-          </Box>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                container
+                justifyContent="center"
+                alignItems="center"
+              >
+                <input
+                  type="email"
+                  required
+                  className="inputBox"
+                  name="email"
+                  id="email"
+                  aria-label="Email Address"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={handleInputChange}
+                ></input>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={12}
+                container
+                justifyContent="center"
+                alignItems="center"
+              >
+                <textarea
+                  type="text"
+                  required
+                  className="inputBox"
+                  name="message"
+                  id="message"
+                  aria-label="Your Message"
+                  placeholder="Your Message"
+                  value={message}
+                  onChange={handleInputChange}
+                ></textarea>
+              </Grid>
+              <div className="button-container">
+                <Grid item xs={12} md={12} justifyContent="center" container>
+                  <button
+                    variant="contained"
+                    type="submit"
+                    // endIcon={<SendIcon style={{ fontSize: "2.75rem" }} />}
+                  >
+                    Send
+                  </button>
+                </Grid>
+              </div>
+            </Grid>
+          </form>
+        </Box>
       </div>
     </>
   );
-  }
+}
